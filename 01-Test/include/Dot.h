ed34cfb9a21f0bd0ab881ab64dc9206b6a5af4bc
Alexis Haldy <alexishaldy@gmail.com>
2022-10-19 13:23:21 +0200

    Add test project

---
+++ 01-Test/include/Dot.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Dot
+{
+public:
+	Dot(int x, int y);
+	~Dot();
+
+private:
+	int _x;
+	int _y;
+};
---
+++ 01-Test/src/Dot.cpp
@@ -0,0 +1,12 @@
+#include "Dot.h"
+
+Dot::Dot(int x, int y)
+{
+	_x = x;
+	_y = y;
+}
+
+Dot::~Dot()
+{
+
+}
